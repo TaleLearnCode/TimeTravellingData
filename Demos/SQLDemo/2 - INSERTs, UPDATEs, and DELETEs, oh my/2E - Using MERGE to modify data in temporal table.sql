f63b477610be55d87f0d719195cbad29e98ac480
Chad Green <chad.green@talelearncode.com>
2022-08-07 11:17:59 -0400

    Correcting Merge example script

--- Demos/SQLDemo/2 - INSERTs, UPDATEs, and DELETEs, oh my/2E - Using MERGE to modify data in temporal table.sql
+++ Demos/SQLDemo/2 - INSERTs, UPDATEs, and DELETEs, oh my/2E - Using MERGE to modify data in temporal table.sql
@@ -8,15 +8,13 @@ statements have regarding PERIOD columns.
 */
 
 DECLARE @DepartmentStaging TABLE (DepartmentId INT, DepartmentName VARCHAR(50));
-GO
 
-INSERT INTO DepartmentStaging VALUES (1,  'Company Management');
-INSERT INTO DepartmentStaging VALUES (10, 'Science & Research');
-INSERT INTO DepartmentStaging VALUES (15, 'Process Management');
-GO
+INSERT INTO @DepartmentStaging VALUES (1,  'Company Management');
+INSERT INTO @DepartmentStaging VALUES (10, 'Science & Research');
+INSERT INTO @DepartmentStaging VALUES (15, 'Process Management');
 
 MERGE Department AS TARGET
-USING (SELECT DepartmentId, DepartmentName FROM DepartmentStaging)
+USING (SELECT DepartmentId, DepartmentName FROM @DepartmentStaging)
 AS SOURCE (DepartmentId, DepartmentName)
 ON (TARGET.DepartmentId = SOURCE.DepartmentId)
 WHEN MATCHED THEN UPDATE SET DepartmentName = SOURCE.DepartmentName
